fb672c6f8f2f9659a12840ca432281506b65cbfe
gigigi <jj1421611@naver.com>
2024-04-02 18:15:40 +0900

    24_0402_first_react

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+export default function Index() {
+    return (
+      <p id="zero-state">
+        <br/>
+        연결의 방식은 진화합니다. 그리고 저희도 변화합니다.
+        <br />
+        모두가 더욱 가까워지는 세상을 만듭니다.
+        <br/>
+        Check out{" "}
+        {/* <a href="https://reactrouter.com"> */}
+        <a href="https://naver.com">
+          the docs at reactrouter.com
+        </a>
+        .
+      </p>
+    );
+  }
